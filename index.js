69872037706dc20616e216c3524264c44bd60018
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-13 20:15:38 +0000

    complete basic hello world example

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res, next) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening');
+});
